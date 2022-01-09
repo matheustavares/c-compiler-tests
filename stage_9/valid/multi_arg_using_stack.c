3f82e2b8ef312f59570cc6427fc50b3f67f51584
Matheus Tavares <matheus.bernardino@usp.br>
2022-01-09 13:15:52 -0300

    stage_9: two extra tests for x86_64 calling conventions
    One test checks that we correctly handle extra arguments that do not go
    on registers, but the stack. The other checks that we do not overwrite
    the argument saved in a register when generating the value for the
    remaining arguments. We use division for this because it uses edx and
    ecx, which respectively store the third and fourth arguments on a
    function call.

---
+++ stage_9/valid/multi_arg_using_stack.c
@@ -0,0 +1,7 @@
+int sum8(int v1, int v2, int v3, int v4, int v5, int v6, int v7, int v8) {
+    return v1 + v2 + v3 + v4 + v5 + v6 + v7 + v8;
+}
+
+int main() {
+    return sum8(1, 2, 3, 4, 5, 6, 7, 8);
+}
---
+++ stage_9/valid/register_reusing_on_args.c
@@ -0,0 +1,7 @@
+int sum(int a, int b, int c, int d, int e, int f) {
+    return a + b + c + d + e + f;
+}
+
+int main() {
+    return sum(1, 1, 3 / 3, 4 / 4, 5 / 5, 6 / 6);
+}
